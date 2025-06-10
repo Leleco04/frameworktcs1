fbe645b0f04d408614a5b08980ef2ec8cb2ce1e7
Leandro Hideki <htwleandro@gmail.com>
2025-06-10 12:53:51 -0300

    Primeiro
    Primeiro

---
+++ src/test/java/com/example/projetoframework/ProjetoFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
