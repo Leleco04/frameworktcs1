92bfae5739c13ffcfaac9190cf3b48d722fc9c23
Leandro Hideki <htwleandro@gmail.com>
2025-06-11 11:22:04 -0300

    Update FuncionarioController.java

--- src/main/java/com/example/projetoframeworktcs/controller/FuncionarioController.java
+++ src/main/java/com/example/projetoframeworktcs/controller/FuncionarioController.java
@@ -1,4 +1,8 @@
 package com.example.projetoframeworktcs.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/funcionario")
 public class FuncionarioController {
 }
