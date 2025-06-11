7decb9a21c9d69f85718479c5d2cbfd74bf58cfc
Leandro Hideki <htwleandro@gmail.com>
2025-06-11 09:43:11 -0300

    Add files via upload

---
+++ src/test/java/com/example/projetoframeworktcs/ProjetoFrameworkTcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoframeworktcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFrameworkTcsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
