971a6dff6c6c47d61bf14b270ae704af60266f38
Leandro Hideki <htwleandro@gmail.com>
2025-06-12 15:52:31 -0300

    Add files via upload

--- src/main/java/com/example/projetoframeworktcs/repository/FuncionarioRepository.java
+++ src/main/java/com/example/projetoframeworktcs/repository/FuncionarioRepository.java
@@ -2,6 +2,8 @@ package com.example.projetoframeworktcs.repository;
 
 import com.example.projetoframeworktcs.model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
 }
--- src/main/java/com/example/projetoframeworktcs/service/FuncionarioService.java
+++ src/main/java/com/example/projetoframeworktcs/service/FuncionarioService.java
@@ -1,4 +1,26 @@
 package com.example.projetoframeworktcs.service;
 
+import com.example.projetoframeworktcs.model.Funcionario;
+import com.example.projetoframeworktcs.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FuncionarioService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    public void save(Funcionario funcionario) {
+        funcionarioRepository.save(funcionario);
+    }
+
+    public long quantidadeFuncionarios() {
+        return funcionarioRepository.count();
+    }
+
+    public Funcionario buscarFuncionarioPorId(Long id) {
+        return funcionarioRepository.findById(id).orElseThrow( () -> new RuntimeException("Funcionário não encontrado."));
+    }
+
 }
