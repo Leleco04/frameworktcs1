c4e782564b9ec22a075d0c0a30d75d850f023739
Guilherme-sitta <124634914+Guilherme-sitta@users.noreply.github.com>
2025-06-13 11:31:08 -0300

    Update FuncionarioController.java

--- src/main/java/com/example/projetoframeworktcs/controller/FuncionarioController.java
+++ src/main/java/com/example/projetoframeworktcs/controller/FuncionarioController.java
@@ -1,19 +1,37 @@
-package com.example.projetoframeworktcs.controller;
+package controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import model.Funcionario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.FuncionarioService;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/funcionarios")
 public class FuncionarioController {
 
-    @GetMapping("/funcionario_inicial")
-    public String paginaInicialFuncionario() {
-        return "funcionario_inicial";
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    @PostMapping
+    public ResponseEntity<Funcionario> criarFuncionario(@RequestParam String nome, 
+                                                        @RequestParam String sobrenome, 
+                                                        @RequestParam String codigoFuncionario, 
+                                                        @RequestParam int idade, 
+                                                        @RequestParam int genero, 
+                                                        @RequestParam Setor setor) {
+        Funcionario funcionario = funcionarioService.criarFuncionario(nome, sobrenome, codigoFuncionario, idade, genero, setor);
+        if (funcionario == null) {
+            return ResponseEntity.badRequest().body(null);
+        }
+        return ResponseEntity.ok(funcionario);
     }
 
-    @GetMapping("/adicionar_funcionario")
-    public String paginaAdicionarFuncionario() {
-        return "adicionar_funcionario";
+    @GetMapping
+    public ResponseEntity<List<Funcionario>> listarFuncionarios() {
+        List<Funcionario> funcionarios = funcionarioService.listarFuncionarios();
+        return ResponseEntity.ok(funcionarios);
     }
-
-    }
+}
