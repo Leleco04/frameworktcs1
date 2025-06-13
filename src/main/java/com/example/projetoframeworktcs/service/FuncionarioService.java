5cc11aaa690bddd5408573d57806555c612f8b31
Guilherme-sitta <124634914+Guilherme-sitta@users.noreply.github.com>
2025-06-13 11:32:52 -0300

    Update FuncionarioService.java

--- src/main/java/com/example/projetoframeworktcs/service/FuncionarioService.java
+++ src/main/java/com/example/projetoframeworktcs/service/FuncionarioService.java
@@ -8,7 +8,6 @@ import java.util.List;
 @Service
 public class FuncionarioService {
 
-    // Exemplo de um método que pode ser chamado para adicionar ou processar a lógica de vendas
     public Funcionario criarFuncionario(String nome, String sobrenome, String codigoFuncionario, int idade, int genero, Setor setor) {
         try {
             return new Funcionario(nome, sobrenome, codigoFuncionario, idade, genero, setor);
@@ -17,10 +16,7 @@ public class FuncionarioService {
             return null;
         }
     }
-
-    // Exemplo de uma lista de funcionários que poderia ser retornada ou manipulada
     public List<Funcionario> listarFuncionarios() {
-        // Este método normalmente faria uma busca no banco de dados, mas aqui é apenas um exemplo
         return List.of(new Funcionario("João", "Silva", "F001", 30, 1, Setor.VENDAS));
     }
 }
