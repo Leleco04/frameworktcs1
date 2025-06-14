2c8c8c81677b77b7f08951a4f7cfcf6f6d11328f
Leandro Hideki <htwleandro@gmail.com>
2025-06-14 10:50:53 -0300

    Update Funcionario.java

--- src/main/java/com/example/projetoframeworktcs/model/Funcionario.java
+++ src/main/java/com/example/projetoframeworktcs/model/Funcionario.java
@@ -1,43 +1,93 @@
 package com.example.projetoframeworktcs.model;
 
-import com.example.projetoframeworktcs.model.enums.Genero;
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.Serializable;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
 public class Funcionario implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    public Funcionario() {
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String sobrenome;
 
-    private Integer idade;
+    private String genero;
 
-    @Enumerated(EnumType.STRING)
-    private Genero genero;
+    private int idade;
 
-    @Embedded
-    private Salario salario;
-    private Integer id_setor;
+    private double salario;
 
-    public Funcionario(String nome, String sobrenome, Integer idade, Genero genero, Salario salario, Integer id_setor) {
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public Setor getSetor() {
+        return setor;
+    }
+
+    public void setNome(String nome) {
         this.nome = nome;
-        this.id_setor = id_setor;
+    }
+
+    public void setSobrenome(String sobrenome) {
         this.sobrenome = sobrenome;
+    }
+
+    public void setGenero(String genero) {
         this.genero = genero;
+    }
+
+    public void setIdade(int idade) {
         this.idade = idade;
+    }
+
+    public void setSalario(double salario) {
         this.salario = salario;
     }
 
+    public void setSetor(Setor setor) {
+        this.setor = setor;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "id_setor")
+    private Setor setor;
+
+    public Funcionario(String nome, String sobrenome, String genero, int idade, Setor setor) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.genero = genero;
+        this.idade = idade;
+        this.setor = setor;
+    }
 }
