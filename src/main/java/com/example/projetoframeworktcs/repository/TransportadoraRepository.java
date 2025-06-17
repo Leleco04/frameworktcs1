5322e8f70ce59ef4ff7db32991a64beabcc3b00e
Graciele Miki Sazaka <gracielesazaka@gmail.com>
2025-06-17 09:07:29 -0300

    Delete src/main/java/com/example/projetoframeworktcs/repository/TransportadoraRepository.java

--- src/main/java/com/example/projetoframeworktcs/repository/TransportadoraRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projetoframeworktcs.repository;
-
-import com.example.projetoframeworktcs.model.Transportadora;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransportadoraRepository extends JpaRepository<Transportadora, Long> {
-}
