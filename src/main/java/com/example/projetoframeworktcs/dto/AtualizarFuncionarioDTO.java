d96e44c02cf9b324e15acbd12f1fcf028a3844c5
Leandro Hideki <htwleandro@gmail.com>
2025-06-17 08:40:12 -0300

    Add files via upload

--- src/main/java/com/example/projetoframeworktcs/dto/AtualizarFuncionarioDTO.java
+++ src/main/java/com/example/projetoframeworktcs/dto/AtualizarFuncionarioDTO.java
@@ -5,12 +5,50 @@ import com.example.projetoframeworktcs.model.enums.Genero;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 public class AtualizarFuncionarioDTO {
     private String nome;
     private String sobrenome;
     private Integer idade;
-    private Genero genero;
-    private Setor setor;
+    private String genero;
+    private long idSetor;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public long getIdSetor() {
+        return idSetor;
+    }
+
+    public void setIdSetor(long idSetor) {
+        this.idSetor = idSetor;
+    }
 }
