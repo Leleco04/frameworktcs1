8cf5a500c58f5ca26d443758178e7e1d83cdc1ba
Leandro Hideki <htwleandro@gmail.com>
2025-06-18 04:28:20 -0300

    Update CaixaController.java

--- src/main/java/com/example/projetoframeworktcs/controller/CaixaController.java
+++ src/main/java/com/example/projetoframeworktcs/controller/CaixaController.java
@@ -8,9 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CaixaController {
 
+    private final NegocioService negocioService;
+
+    public CaixaController(NegocioService negocioService) {
+        this.negocioService = negocioService;
+    }
+
     @GetMapping("/caixa")
-    public String caixa(Model model) {
+    public String caixa(Model model, @PageableDefault(size = 5, sort = "id") Pageable pageable) {
         model.addAttribute("caixa", Caixa.getValor());
+        model.addAttribute("negocios", negocioService.getNegocios(pageable));
+        model.addAttribute("qtdNegocios", negocioService.quantidadeNegocios());
         return "caixa";
     }
 }
